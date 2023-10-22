2ead02a2c73531eef250dbec62910818f024bc9e
Mehdi Dehbozorgi <113126374+mehdi530@users.noreply.github.com>
2023-10-23 00:28:26 +0330

    add project file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const express = required("express")
+const express = require("express");
+const app = express();
+const http = require("http");
+http.createServer(app);
+const router = require("./routes/home");
+
+app.use(express.json());
+require("dotenv").config();
+
+app.get("/", router);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`connect to port ${port}`);
+});
