1857123a2f15e2675c773bc6ce1609c17b5a9bd1
Mehdi Dehbozorgi <113126374+mehdi530@users.noreply.github.com>
2023-10-23 00:31:32 +0330

    Create functions.js
    add controllers file

---
+++ controllers/functions.js
@@ -0,0 +1,23 @@
+function gettime(){
+
+}
+
+function getdate(){
+    
+}
+
+function print_all_array(input1){
+     
+   
+     for (let index = 0; index < input1.length  ; index++) {
+        
+         console.log(input1[index]);
+
+       
+     }
+
+
+}
+
+
+module.exports = {print_all_array}
